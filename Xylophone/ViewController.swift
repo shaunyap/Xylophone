d3db9a8b99f68b9af065eac0d4e7de2390d17dad
Shaun Yap <shaun@siegeflow.com>
2018-12-01 09:50:01 +0800

    Initial Commit

---
+++ Xylophone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Shaun Yap on 1/12/18.
+//  Copyright © 2018 siegeflow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
